258bf5c078c9b8e9c3ae409837aa01dd4cb42717
kathmarioly <katerinemarioly@gmail.com>
2017-07-13 22:34:35 -0400

    Section2: siderbar Navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-/* Holiii acá va tu código también */
+
+$('.form-group').validate({
+	rules : {
+		password : {
+			required: true,
+			minlength: 6,
+			maxlength: 10
+		},
+		password_confirm : {
+			required: true,
+			equalTo: "#pwd",
+			minlength: 6,
+			maxlength: 10
+		}
+	}
+});
+
+
+
+/*index 2*/
+
+
+// SideNav Initialization
+$(".button-collapse").sideNav();
+
+
+
+/*FIn index 2*/
